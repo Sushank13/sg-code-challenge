4c88ca765329d0d8a3b55876bd12d08fa136e3f7
Sushank Saini <sushank.saini@dal.ca>
2024-11-02 15:35:47 -0400

    refactor: parsing of input data in adduserdetails.py

--- lambdas/adduserdetails.py
+++ lambdas/adduserdetails.py
@@ -4,13 +4,14 @@ import os
 
 print("Initializing DynamoDB client")
 dynamodb=boto3.client("dynamodb")
-table_name=os.environ["DYNAMODB_TABLE_NAME"]
+table_name=os.environ["DYNAMODB_TABLE"]
 
 def adduserdetails(event,context):
     print("Entered inside function.")
+    body=json.loads(event["body"])
     print("Parsing input data from the Lambda event")
-    user_email=event["user_email"]
-    user_full_name=event["user_full_name"]
+    user_email=body["user_email"]
+    user_full_name=body["user_full_name"]
     
     try:
         if(user_email is None or user_full_name is None): # input validation 
@@ -24,7 +25,7 @@ def adduserdetails(event,context):
             TableName=table_name,
             Item={
                 "user_email": {"S": user_email},
-                "user_full_name": {"S": user_email}
+                "user_full_name": {"S": user_full_name}
             }
         )
         if (db_response is not None):
